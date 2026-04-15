0f730dc2d4c952e880524691d3aa6b8f53b73cb6
Darshan G.V <darshangv36@gmail.com>
2026-04-16 01:17:59 +0530

    remaining file

---
+++ stone.cpp
@@ -0,0 +1,10 @@
+// LeetCode Number : 877. Stone Game
+
+// when even number of piles arranged in a row , whoever pick the stones they will be the winner
+
+    class Solution {
+      public:
+    bool stoneGame(vector<int>& piles) {
+        return true;
+    }
+};
